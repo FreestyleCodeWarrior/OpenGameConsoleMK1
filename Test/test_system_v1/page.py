6d3c74a4eb078fe7fd74cae8da2e52b365c86a76
Hell0w0rld <2844933250@qq.com>
2022-10-19 22:01:13 +0800

    test the system in test section

---
+++ Test/test_system_v1/page.py
@@ -0,0 +1,7 @@
+# This class consisting with required information of pages for user-device interaction
+# MicroPython version: v1.19.1 on 2022-06-18
+# Espressif ESP32-WROOM-32
+
+class Page:
+    def __init__(self):
+        pass
